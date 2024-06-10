23aa63978e453be2c7e69bae4d7a910505a3a93b
cmdsiraj <cmdsiraj2003@gmail.com>
2024-06-10 19:45:20 +0530

    first

---
+++ Client/Client/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                using (var client = new TcpClient("127.0.0.1", 11000))
+                using (var stream = client.GetStream())
+                {
+
+                    Console.Write("You: ");
+                    var message = Console.ReadLine();
+                    if (message == null || string.Compare(message, "end", StringComparison.OrdinalIgnoreCase) == 0)
+                    {
+                        break;
+                    }
+
+                    var messageBytes = Encoding.ASCII.GetBytes(message);
+                    stream.Write(messageBytes, 0, messageBytes.Length);
+
+                    var buffer = new byte[1024];
+                    var byteCount = stream.Read(buffer, 0, buffer.Length);
+                    var response = Encoding.ASCII.GetString(buffer, 0, byteCount);
+                    Console.WriteLine($"Reponse: {response}");
+                
+            }
+        }
+            Console.WriteLine("Connection Closed. Bye!");
+        }
+    }
+}
---
+++ Listener/Listener/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Listener
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintIpAddress();
+            var listener = new TcpListener(IPAddress.Any, 11000);
+            listener.Start();
+            Console.WriteLine("Waiting for a Connection...");
+            while(true)
+            {
+                using (var client = listener.AcceptTcpClient())
+                {
+                    using(var stream = client.GetStream())
+                    {
+                            var buffer = new byte[1024];
+                            var byteCount = stream.Read(buffer, 0, buffer.Length);
+                            var request = Encoding.ASCII.GetString(buffer, 0, byteCount);
+                            Console.WriteLine($"Received message: {request}");
+
+                            Console.Write("You: ");
+                            var message = Console.ReadLine();
+                            if(message==null || string.Compare(message, "end", StringComparison.OrdinalIgnoreCase) == 0)
+                            {
+                                break;
+                            }
+                            var response = Encoding.ASCII.GetBytes(message);
+                            stream.Write(response, 0, response.Length);
+                        
+                    }
+                }
+            }
+            Console.WriteLine("Closing Connection. Bye!");
+        }
+
+
+        static void PrintIpAddress()
+        {
+            var hostName = Dns.GetHostName();
+            Console.WriteLine($"Host Name: {hostName}");
+
+            IPAddress[] IPAddresses = Dns.GetHostAddresses(hostName);
+            foreach(var ipAddress in IPAddresses)
+            {
+                if(ipAddress.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    Console.WriteLine($"Ip Address: {ipAddress}");
+                }
+            }
+        }
+    }
+}
